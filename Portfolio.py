365427d6134c7afd18c1469f97d978af2e034808
Cody Cao <codyjcao@gmail.com>
2019-02-22 01:09:30 -0500

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -402,7 +402,7 @@ class Portfolio :
             indextoplot = 100*i_ts/i_ts.iloc[0]
             if returns:
                 indextoplot = compute_returns(i_ts,log)
-            return _benchmark(toplot, indextoplot, plot, sigmas,normalize)
+            return benchmark_help(toplot, indextoplot, plot, sigmas,normalize)
         else:
             try:
                 print("Getting rates from the Fed")
@@ -411,7 +411,7 @@ class Portfolio :
                 print("Failed to get rates from the Fed")
                 raise Exception ("Not a valid interest rate")
             port_rets = compute_returns(port_vals)
-            return _benchmark(port_rets, rates, plot, sigmas, normalize, True)
+            return benchmark_help(port_rets, rates, plot, sigmas, normalize, True)
     
     def sharpe_ratio_ptf(self, first='today', last='today', rebalancing = True):
         from scipy.stats.mstats import gmean
